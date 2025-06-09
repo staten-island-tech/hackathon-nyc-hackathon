af25c6f7e7ba66fd1f9f34d872493ea58469e778
Janet35 <janetl35@nycstudents.net>
2025-06-09 09:46:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,15 @@ from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    
+    elif request.method == "POST":
+        search = request.form.get("search")
+        url = f"http://www.rotaryspin.com/mc/api/beatles.php?query={search}"
+        response =requests.get(url)
+        response.raise_for_status()
+        data = response.json()

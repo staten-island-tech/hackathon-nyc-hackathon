1f16fdd0f41a4586e59d265dd69153c430c272d3
Janet35 <janetl35@nycstudents.net>
2025-06-13 10:16:55 -0400

    commit

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    role = random.choice(['Police', 'Homeless'])
-    return render_template('index.html', role=role)
+@app.route("/")
+def game():
+    return render_template("flappy_nyc.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
